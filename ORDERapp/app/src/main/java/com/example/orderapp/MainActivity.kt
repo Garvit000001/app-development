40244c8a92fa9ac3e43aa657b7d894c13ac6188e
Riya Gupta <ggarvit97@gmail.com>
2023-02-22 01:58:53 +0530

    made ui ux for app

--- ORDERapp/app/src/main/java/com/example/orderapp/MainActivity.kt
+++ ORDERapp/app/src/main/java/com/example/orderapp/MainActivity.kt
@@ -20,13 +20,16 @@ class MainActivity : AppCompatActivity() {
 
         btnOrder.setOnClickListener {
             //isme et1.gettext.tostring()
+            //pehle humne order place name ka val banaya jisme message store h jo hum dikhaenge in next page
             val orderplaced = et1.text.toString() + " " + et2.text.toString() + " " +
                     et3.text.toString() + " " + et4.text.toString()
 
 
 //in kotlin companion object works like static of java ie global variable bnane ke kam ata hai
+            //can direct write intent like this or we can also create val intent which is also the right way
             intent = Intent(this, ShowOrder::class.java)
             intent.putExtra(KEY, orderplaced)//these are key value pairs which we have given inside it using comma
+//            where orderplaced is value and key is key very simple to understand
             startActivity(intent)
 
 
--- ORDERapp/app/src/main/java/com/example/orderapp/ShowOrder.kt
+++ ORDERapp/app/src/main/java/com/example/orderapp/ShowOrder.kt
@@ -8,7 +8,7 @@ class ShowOrder : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_order)
-
+//is bar hume pehle wale activity se data lena h ie to read that data ie  intent se data lena hai
 //getintent use kiya jo khud intent ke anddar tha so intent me hi badal gya
         val ordersofCustomer=intent.getStringExtra(MainActivity.KEY)
         //textvieworder.gettext kiya fir
