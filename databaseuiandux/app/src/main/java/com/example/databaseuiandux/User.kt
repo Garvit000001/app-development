0ccf24b32520dc6557be8b94090bcf6c6191b68a
Riya Gupta <ggarvit97@gmail.com>
2023-02-22 19:35:09 +0530

    learned and implemented firebase first time

---
+++ databaseuiandux/app/src/main/java/com/example/databaseuiandux/User.kt
@@ -0,0 +1,5 @@
+package com.example.databaseuiandux
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+
+data class User(val Name:String,val email: String,val Password: String,val UniqueId: String)
